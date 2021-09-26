9dad4716b3be1d11c2ff0da1a38273fc852e4e78
ziadycb <zarayaacoub@hotmail.com>
2021-09-26 23:07:14 +0300

    main

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+window.onload=function(){
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list .drop');
 const header = document.querySelector('.header.container');
+const Home = document.getElementById("Home");
+const About = document.getElementById("About");
+const Contact = document.getElementById("Contact");
+
+Home.addEventListener("click", handler);
+About.addEventListener("click", handler);
+Contact.addEventListener("click", handler);
 
 hamburger.addEventListener('click', () => {
-hamburger.classList.toggle('active');
-mobile_menu.classList.toggle('active');
+    console.log("Hello world!");
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    
 });
+
+function handler() {
+    console.log("Hello world!");
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+}
+}
+
